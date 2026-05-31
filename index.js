750aa41370b21d31655b6bb8a94ae654072277aa
HariniWeerarathna <hariniiweerarathna@gmail.com>
2026-06-01 00:11:04 +0530

    Removing unnecessary codes

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 import express from 'express' 
-import mongoose from 'mongoose'  
-import studentRouter from './routes/studentRouter.js' 
+import mongoose from 'mongoose'   
 import userRouter from './routes/userRouter.js'
 import authenticateUser from './middlewares/authenticate.js'
 import productRouter from './routes/productRouter.js'
@@ -19,7 +18,6 @@ mongoose.connect(mongourl).then(
 app.use(express.json()) // stop request - make Inorder(piliwelata haduwa) ---> to read json data from request body.
 
 app.use( authenticateUser)
-app.use("/students",studentRouter)
 app.use("/users",userRouter)
 app.use("/products", productRouter)
 
