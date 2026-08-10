08f9c25b3bf978fbb4607a71b352d75edfcd0c13
HariniWeerarathna <hariniiweerarathna@gmail.com>
2026-08-10 21:21:02 +0530

    Update order and user management

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,9 +1,13 @@
 import express from 'express';
-import {createUser} from '../controllers/userController.js';
-import {loginUser} from '../controllers/userController.js';
+import {createUser, getAllUsers, getCurrentUser, loginUser, updateUserRole, updateUserStatus} from '../controllers/userController.js';
 
 const userRouter = express.Router();
+
+userRouter.get("/me", getCurrentUser)
 userRouter.post("/",createUser)
 userRouter.post("/login", loginUser)
+userRouter.get("/:pageSize/:pageNumber", getAllUsers)
+userRouter.put("/role", updateUserRole)
+userRouter.put("/status", updateUserStatus)
 
 export default userRouter
